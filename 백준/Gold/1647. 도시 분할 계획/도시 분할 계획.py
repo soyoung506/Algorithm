959205619e62d32e8e464622f76f373e93aa8753
soyoung <106011129+soyoung506@users.noreply.github.com>
2024-01-20 19:21:02 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2276 ms, Memory: 276484 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,47 @@
+import sys
+
+
+class DisjointSet:
+
+    def __init__(self, n):
+        self.parent = list(range(n + 1))
+
+    def find(self, x):
+        if self.parent[x] == x:
+            return x
+        self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x == root_y:
+            return
+        self.parent[root_x] = root_y
+
+
+n, edgeN = map(int, sys.stdin.readline().split())
+edges = []
+
+for _ in range(edgeN):
+    edge = list(map(int, sys.stdin.readline().split()))
+    edges.append(edge)
+
+edges.sort(key=lambda x: x[2])
+disjoint_set = DisjointSet(n)
+result = 0
+used_edges = 0
+selected = []
+
+for idx, adj, cost in edges:
+    if idx == adj:
+        continue
+
+    if disjoint_set.find(idx) != disjoint_set.find(adj):
+        disjoint_set.union(idx, adj)
+        result += cost
+        selected.append(cost)
+
+result -= selected.pop()
+
+print(result)
