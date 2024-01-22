dffc878befa2c1fcdb2eaddc7c82b0e0c2efbcd8
soyoung <106011129+soyoung506@users.noreply.github.com>
2024-01-23 03:15:03 +0900

    [Silver I] Title: 신입 사원, Time: 4088 ms, Memory: 210764 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,26 +1,22 @@
 import sys
 
 t = int(input())
-tList = list()
-result = list()
 
 for _ in range(t):
-    recruitList = list()
     n = int(input())
+    recruitList = list()
+    count = 1
+    minimum = 0
+
     for _ in range(n):
         document, interview = map(int, sys.stdin.readline().split())
         recruitList.append((document, interview))
 
     recruitList.sort()
-    tList.append(recruitList)
 
-for testCase in tList:
-    count = 0
-    minimum = 0
-    for applicant in testCase:
+    for applicant in recruitList:
         if applicant[0] == 1:
             minimum = applicant[1]
-            count += 1
             continue
 
         if minimum > applicant[1]:
@@ -28,9 +24,6 @@ for testCase in tList:
             count += 1
 
         if minimum == 1:
-            result.append(count)
             break
 
-for case in result:
-    print(case)
-    
+    print(count)
