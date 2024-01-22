1d463e8524d70a79cd1e09c27ed2ecd09791bb72
soyoung <106011129+soyoung506@users.noreply.github.com>
2024-01-23 02:26:50 +0900

    [Silver IV] Title: ATM, Time: 104 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,13 +1,13 @@
-import heapq
 import sys
 
 n = int(input())
 timeList = list(map(int, sys.stdin.readline().split()))
+timeList.sort()
 result = list()
 count = 0
 
-for i in range(1, n + 1):
-    count += heapq.nsmallest(i, timeList)[-1]
+for i in timeList:
+    count += i
     result.append(count)
 
 print(sum(result))
